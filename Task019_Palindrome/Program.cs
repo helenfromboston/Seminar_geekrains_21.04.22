af4d15d0666f73392342e20e21a4dff01d3cae6d
helenfromboston <helenfromboston@gmail.com>
2022-04-21 20:04:11 +0300

    Added tasks 019, 021, 023

---
+++ Task019_Palindrome/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100000 && number > 9999)
+{
+    if (number % 10 == number / 10000 && number % 100 / 10 == number / 1000 % 10) Console.Write("Да");
+    else Console.Write("Нет");
+}
+else Console.Write("Введено не пятизначное число");
---
+++ Task023_Cube/Program.cs
@@ -0,0 +1,11 @@
+﻿// на вход число N, на выходе таблица кубов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int cube = 1;
+
+while (cube<=N)
+{
+    Console.WriteLine(Math.Pow(cube, 3));
+    cube++;
+}
